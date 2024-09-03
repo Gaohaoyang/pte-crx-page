842db0d20552dcbab38cd538b0e84d134ee9830a
Haoyang Gao <gaohaoyang126@126.com>
2024-09-02 23:04:56 -0400

    feat: 🎸 update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
 import { MdStar } from 'react-icons/md'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import { Autoplay, Pagination } from 'swiper/modules'
+
 function App() {
   return (
     <>
@@ -37,15 +42,51 @@ function App() {
         Take control of your PTE journey today!
       </div>
 
-      <div className="mt-8 md:flex md:flex-row-reverse md:justify-between gap-6">
-        <div className="h-max w-full md:w-3/5">
-          <img
+      <div className="mt-2 gap-0 md:flex md:flex-row-reverse md:justify-between">
+        <div className="relative left-[-1rem] h-max w-screen md:left-0 md:w-2/3">
+          <Swiper
+            pagination={{
+              bulletActiveClass: 'swiper-pagination-bullet-active bg-sky-700',
+            }}
+            autoplay={{
+              delay: 2600,
+            }}
+            modules={[Pagination, Autoplay]}
+            style={{}}
+            spaceBetween={0}
+            slidesPerView={1}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+            loop
+          >
+            {[
+              'https://cdn.jsdelivr.net/gh/Gaohaoyang/pics/pte/2.png',
+              'https://cdn.jsdelivr.net/gh/Gaohaoyang/pics/pte/0.png',
+              'https://cdn.jsdelivr.net/gh/Gaohaoyang/pics/pte/1.png',
+              // 'https://cdn.jsdelivr.net/gh/Gaohaoyang/pics/pte/ptePanel.jpg',
+            ].map((item) => (
+              <SwiperSlide className="px-2 pb-8 md:px-5 md:py-8">
+                {/* <div
+                  className="h-96 w-full rounded-lg bg-contain bg-center bg-no-repeat shadow-lg shadow-sky-950/55"
+                  style={{
+                    backgroundImage: `url(${item})`,
+                  }}
+                /> */}
+                <img
+                  className="w-full rounded-lg shadow-lg shadow-sky-950/55"
+                  src={item}
+                  alt=""
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          {/* <img
             className="w-full rounded-xl shadow-2xl shadow-sky-950/85"
             src="https://cdn.jsdelivr.net/gh/Gaohaoyang/pics/pte/2.png"
             alt=""
-          />
+          /> */}
         </div>
-        <div className="mt-8 flex flex-col justify-center text-lg md:mt-0 md:w-2/5">
+        <div className="mt-2 flex flex-col justify-center text-lg md:mt-0 md:w-2/5">
           <p className="mb-6 md:mb-10">
             Simply visit your PTE score page after installation, where you’ll
             find a detailed breakdown of your performance, including your
