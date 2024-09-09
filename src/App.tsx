a70152853a7f136900bc3089f326f8afc29d9b4c
Haoyang <gaohaoyang126@126.com>
2024-09-08 23:16:58 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -246,21 +246,7 @@ function App() {
         </div>
       </div>
 
-      <script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9139027259917346"
-        crossOrigin="anonymous"
-      ></script>
 
-      <ins
-        class="adsbygoogle"
-        style="display:block"
-        data-ad-client="ca-pub-9139027259917346"
-        data-ad-slot="8680029933"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-      ></ins>
-      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
 
       <div className="h-8"></div>
       <DiscussionEmbed
