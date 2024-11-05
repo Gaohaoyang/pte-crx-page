8f1b365d3fb9c644f486bebe90a43f8ac6a64ddf
Haoyang Gao <gaohaoyang126@126.com>
2024-11-05 14:07:38 -0500

    feat: 🎸 navbar

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  images: { unoptimized: true },
 }
 
-export default nextConfig;
+export default nextConfig
