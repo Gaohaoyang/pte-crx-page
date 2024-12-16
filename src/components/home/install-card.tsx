a20479267238f39010f8f2e5ddc7d8b9c3e04213
Haoyang Gao <gaohaoyang126@126.com>
2024-12-16 18:08:38 -0500

    feat: 🎸 hover animation

--- src/components/home/install-card.tsx
+++ src/components/home/install-card.tsx
@@ -2,43 +2,62 @@ import { Button } from '@nextui-org/button'
 import { MdStar } from 'react-icons/md'
 import Image from 'next/image'
 import Logo from '@/assets/home/logo.svg'
+import { CardBody, CardContainer, CardItem } from '@/components/ui/3d-card'
 
 const InstallCard = () => {
   return (
-    <div className="flex flex-1 flex-col items-center justify-between p-2">
-      <div className="flex items-center justify-center gap-2">
-        <Image
-          src={Logo}
-          alt="logo"
-          width={80}
-          height={80}
-          className="h-20 w-20 rounded-md"
-        />
-        <div>
-          <h1 className="text-xl font-semibold md:text-xl">
-            PTE Sub-Scores Breakdown
-          </h1>
-          <h2>Chrome Extension</h2>
-          <div className="mt-1 flex items-center text-gray-600">
-            {[1, 2, 3, 4, 5].map((item) => (
-              <MdStar key={item} className="text-xl dark:text-yellow-500" />
-            ))}
+    <CardContainer containerClassName="flex flex-1" className="inter-var">
+      <CardBody className="group/card relative flex w-full flex-1 flex-col items-center justify-between rounded-xl border border-black/[0.1] p-2 shadow-md transition-shadow duration-300 hover:shadow-2xl dark:border-white/[0.2] dark:bg-slate-800 dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1]">
+        <div className="inter-var mt-1 flex items-center justify-center gap-2">
+          <CardItem translateZ={90}>
+            <Image
+              src={Logo}
+              alt="logo"
+              width={80}
+              height={80}
+              className="h-20 w-20 rounded-md shadow-lg"
+            />
+          </CardItem>
+          <div className="inter-var [transform-style:preserve-3d] [&>*]:[transform-style:preserve-3d]">
+            <CardItem
+              translateZ={90}
+              className="text-xl font-semibold drop-shadow-lg md:text-xl"
+            >
+              PTE Sub-Scores Breakdown
+            </CardItem>
+            <CardItem translateZ={80} className="drop-shadow-lg">
+              Chrome Extension
+            </CardItem>
+            <div className="mt-1 flex items-center text-gray-600">
+              {[1, 2, 3, 4, 5].map((item, index) => (
+                <CardItem
+                  translateZ={240 + 10 * Math.min(index, 4 - index)}
+                  translateX={20}
+                  key={item}
+                  className="text-xl drop-shadow-lg dark:text-yellow-500"
+                >
+                  <MdStar />
+                </CardItem>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
-      <Button
-        color="primary"
-        className="w-full rounded-full"
-        onPress={() => {
-          window.open(
-            'https://chromewebstore.google.com/detail/pte-sub-scores-breakdown/hibclclepijigjnfdkmkfhjogfhgicda',
-            '_blank',
-          )
-        }}
-      >
-        Install from Chrome Web Store
-      </Button>
-    </div>
+        <CardItem translateZ={80} className="flex w-full justify-center">
+          <Button
+            color="primary"
+            className="w-11/12 rounded-full font-semibold shadow-lg"
+            onPress={() => {
+              window.open(
+                'https://chromewebstore.google.com/detail/pte-sub-scores-breakdown/hibclclepijigjnfdkmkfhjogfhgicda',
+                '_blank',
+              )
+            }}
+          >
+            Install from Chrome Web Store
+          </Button>
+        </CardItem>
+      </CardBody>
+    </CardContainer>
   )
 }
 
